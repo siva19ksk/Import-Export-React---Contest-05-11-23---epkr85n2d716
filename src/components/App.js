3eb7bc24209ffecb495cb1ae07895c8daa83a24f
siva19ksk <97792618+siva19ksk@users.noreply.github.com>
2023-11-05 10:37:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,14 @@ import React, { Component, useState } from "react";
 import '../styles/App.css';
 import Heading from "./components/Heading.js";
 import Input from "./components/InputQuery.js";
-import sub from "./components/SubHeading.js";
+import SubHeading from "./components/SubHeading.js";
 import Submit from "./components/SubmitButton.js";
 
 const App = () => {
   return (
     <div id="main">
       <Heading/>
-      <sub/>
+      <SubHeading/>
       <Input/>
       <Submit/>
     </div>
